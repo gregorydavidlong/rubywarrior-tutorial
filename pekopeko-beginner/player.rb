8a5a8015daaf6786001f42db24f147302605a17d
Gregory Long <gregorydavidlong@gmail.com>
2012-02-08 12:48:53 +1100

    Finished level 8

--- pekopeko-beginner/player.rb
+++ pekopeko-beginner/player.rb
@@ -7,18 +7,30 @@ class Player
 
   def play_turn(warrior)
     initialise(warrior)
-    
-    if @change_direction.nil?
-        @change_direction = false
-        warrior.pivot!(:backward)
-    elsif @change_direction == true
-        warrior.pivot!(:forward)
-        change_direction = false
-    elsif @under_attack
-        handle_attack(warrior) 
-    else
-        rest_then_move(warrior)
-    end
+   
+    @forward_look = warrior.look(:forward)
+    @left_look = warrior.look(:left)
+    @right_look = warrior.look(:right)
+    @backward_look = warrior.look(:backward)
+
+    puts "Forwards: " + @forward_look.inspect
+    puts "Left: " + @left_look.inspect
+    puts "Right: " + @right_look.inspect
+    puts "Backwards: " + @backward_look.inspect
+
+    make_a_move(warrior)
+
+    #if @change_direction.nil?
+    #    @change_direction = false
+    #    warrior.pivot!(:backward)
+    #elsif @change_direction == true
+    #    warrior.pivot!(:forward)
+    #    change_direction = false
+    #elsif @under_attack
+    #    handle_attack(warrior) 
+    #else
+    #    rest_then_move(warrior)
+    #end
 
     @health = warrior.health
   end
@@ -29,19 +41,40 @@ class Player
         @health = warrior.health
     end
 
-
-
     @target_direction = :forward
     @alt_direction = :backward
 
     @front_space = warrior.feel(@target_direction)
     @rear_space = warrior.feel(@alt_direction)
 
-    puts "Forwards is ", @front_space
-    puts "Backwards is ", @rear_space
+    #puts "Forwards is ", @front_space
+    #puts "Backwards is ", @rear_space
 
     #are we under attack?
     @under_attack = under_attack?(warrior)
+  end
+
+  def make_a_move(warrior)
+    if (should_attack?(warrior, @forward_look))
+        warrior.shoot!
+    elsif (@forward_look[0].empty?)
+        warrior.walk!
+    elsif (@forward_look[0].captive?)
+        warrior.rescue!
+    end
+  end
+
+  def should_attack?(warrior, look)
+    should_attack = false
+    look.reverse.each{ |space|
+      if (space.enemy?)
+        should_attack = true
+      elsif (space.captive?)
+        should_attack = false
+      end
+    }
+    puts "final should attack: " + should_attack.to_s
+    should_attack
   end
 
   def under_attack?(warrior)
